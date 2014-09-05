067c6415f0ac3244d6c13f741054e4f1794d5388
Brian Woodward <brian.woodward@gmail.com>
2014-09-05 10:56:00 -0400

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * handlebars-helper-unsplash <https://github.com/helpers/handlebars-helper-unsplash>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
